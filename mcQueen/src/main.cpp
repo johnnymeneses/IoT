eb1b9759c731b2d67241680a88b5d34b6620af8a
johnnymeneses <31022621+johnnymeneses@users.noreply.github.com>
2020-12-18 23:44:38 -0300

    Add files via upload

---
+++ mcQueen/src/main.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+
+
+// v0.1 - Rodas configuradas via Push Button para frente e para trás
+// v0.2 - Carro anda pra frente, anda pra trás e para
+// v0.3 - Carro faz curva, anda de ré e para. 
+// v0.4 - Instalação do Sensor. Reconnhece o obstáculo e para
+// v0.5 - Andar pra frente, pra tras e quando reconhecer o obstáculo: volta
+// v1.0 - Primeira versão do carro
+// v1.1 - Instalação do Sensor de Luz e leitura
+// v1.2 - Instalação barra de LED para farol
+// v2.0 - Segunda versão do carro
+
+
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void acionaRoda()
+{
+  // Colocar codigo aqui
+}
+
+
+
+
+void loop() 
+{
+  acionaRoda();
+}
